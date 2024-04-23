0d8b0a47bd364fa81fb51e0b2b1db3fd2aa6bc58
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-22 23:02:51 -0500

    Create Letter.py

---
+++ Letter.py
@@ -0,0 +1,10 @@
+#Carta
+Print("×~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~×")
+Print("                                               Abril 2023")
+Print("                                           Azuay, Ecuador")
+Print("Querida yo del futuro,                                   ")
+Print("   Espero que sigas feliz y ya puedas dominar esta       ")
+Print("   herramienta muy util para nosotras en el futuro       ")
+Print("   sigue triunfando y cuidate                            ")
+Print("                                                         ")
+Print("                                            Angy González")
