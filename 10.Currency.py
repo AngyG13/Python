e66fc863cc7c5464252fd423a8647fc676ec1fc5
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-23 22:19:36 -0500

    Create 10.Currency.py

---
+++ 10.Currency.py
@@ -0,0 +1,8 @@
+#Currency
+
+Pesos = int(input("What do you have left in pesos? "))
+Soles = int(input("What do you have left in soles? "))
+Reals = int(input("What do you have left in reals? "))
+
+Total = pesos * 0.00025 + soles * 0.28 + reals * 0.21
+Print(total)
