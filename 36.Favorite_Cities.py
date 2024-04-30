88601ec83e5e3cfb4867850d942fe490ecd14764
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 18:36:06 -0500

    Actualizar 36.Favorite_Cities.py

--- 36.Favorite_Cities.py
+++ 36.Favorite_Cities.py
@@ -1,14 +1,2 @@
 # Favorite Cities 🌆
 
-🔽 class City:
-🔽   def __init__(self, name, country, population, landmarks):
-       self.name = name
-       self.country = country
-       self.population = population
-       self.landmarks = landmarks
-
-nyc = City("New York City", "USA", 8468000, ["Statue of Liberty", "Brooklyn Bridge", "Apollo Theatre"])
-shangai = City("Shangai", "China", "26320000, ["The Bund", "Jin Mao Tower", "Tianzifang"])
-
-print(vars(nyc))
-print(vars(shangai))
