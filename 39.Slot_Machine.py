a7053c56b86db6bd3450ff8e8fd374e2c017990c
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 18:34:58 -0500

    Actualizar 39.Slot_Machine.py

--- 39.Slot_Machine.py
+++ 39.Slot_Machine.py
@@ -1,36 +1,3 @@
-# Slot Machine 💰
-
-import random
-
-🔽 simbols = [
-        "🍒",
-        "🍇",
-        "🍉",
-        "7️⃣"
-        ]
-    
-🔽 def play():
-    for i in range (1, 51):
-        results = random.choices (symbols, k=3)
-        print(f"{results[0]} | {results[1]} | {results[2]}")
-        win = results[0]
-
-if win:
-results = random.choices (symbols, k=3)
-print(f'{results[0]} | {results[1]} | {results[2]}")
-win = results[0] == "7️⃣" and results[1] == "7️⃣" and results[2] == "7️⃣"
-
-if win:
-    print("Jackpot!!! 💰")
-    break
-    else:
-    results = random.choices(symbols, k=3)
-    
-answer = ''
-while answer.upper() != "N":
-    play()
-    answer = input("keep playing? (Y/N) ")
-    
-print("Thanks for playing!")
+# Slot_Machine.py
 
         
