6fcb6819431338ba96c54bfe685818e4342f70b5
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 02:47:57 -0500

    Actualizar 38.Pokedex.py

---
+++ 38.Pokedex.py
@@ -0,0 +1,27 @@
+# Pokédex 📟
+
+# class definition
+🔽 class Pokemon:
+🔽   del __init__(self, entry, name, types, description, level, region, is_caught):
+         self.entry = entry
+         self.name = name
+         self.types = types
+         self.description + description
+         self.level = level
+         self.region = region
+         self.is_caught = is_caught
+        
+    def speak(self):
+       print("self.name + ". " + self.name +"!")
+    def display_details(self):
+       print("Entry number: "+ str(self.entry))
+       print("Name: " + self.name)
+    if len(self.types == 1:
+     print("Type: " + self.types[0])
+    else:
+     print("Lv. " + str(stelf.types[0] + " / " + self. types[1])
+    
+    print('lv. ' + str(self.level))
+    print( 'Region: ' + self. region)
+    print( 'Description: ' + self description)
+    
