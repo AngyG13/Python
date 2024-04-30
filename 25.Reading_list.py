b2d7bd5c4dc62054f53d228263bff7faef6bde5c
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 18:40:57 -0500

    Actualizar 25.Reading_list.py

--- 25.Reading_list.py
+++ 25.Reading_list.py
@@ -1,13 +1,2 @@
 # Reading List 📚
 
-🔽 Books = ["Zero to one",
-            "The Lean Startup",
-            "The Mom test",
-            "Made to Stick",
-            "Life in Code"]
-  print(books)
-
- books.append("Zero to Sold")
- books.remove("Zero to One")
- books.pop(0)
-  print(books)
