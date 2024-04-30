d104ce919fcadd74d5769ada21dbf92dc563af71
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 18:41:43 -0500

    Actualizar 22.Grocery.py

--- 22.Grocery.py
+++ 22.Grocery.py
@@ -1,9 +1,2 @@
 # Grocery Lists
 
-🔽 grocery = ["🥚 Eggs",
-              "🥑 Avocados",
-              "🍪 Cookies",
-              "🌶️ Hot Pepper Jam",
-              "🫐 Blueberries",
-              "🥦 Broccoli"]
-print(grocery)
