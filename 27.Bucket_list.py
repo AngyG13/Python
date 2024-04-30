6995fb493cebdba0a3f416db9d6d62d978cfc706
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-29 22:28:49 -0500

    Actualizar 27.Bucket_list.py

---
+++ 27.Bucket_list.py
@@ -0,0 +1,13 @@
+# Bucket List: Jerry Zh
+
+🔽 things_to_do = [
+   "🚀 Build a menaingful product for everyone.", 
+   "⛰️ Try out hiking and mountain biking.",
+   "🌏 Visit at least 10 countries in my lifetime.",
+   "🎸 Produce an original song.",
+   "📝 Write a short story.",
+   "🏃🏻‍♀️ Finish a 10k marathon."
+ ]
+
+for thing in things_to_do: 
+  print(thing)
