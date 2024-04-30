7e574f79c1123ae3319a7d3937512c2bc25fb2de
AngyG13 <167836385+AngyG13@users.noreply.github.com>
2024-04-30 08:17:15 -0500

    Actualizar 38.Pokedex.py

--- 38.Pokedex.py
+++ 38.Pokedex.py
@@ -19,9 +19,22 @@
     if len(self.types == 1:
      print("Type: " + self.types[0])
     else:
-     print("Lv. " + str(stelf.types[0] + " / " + self. types[1])
+     print("Type: " + stelf.types[0] + "/" + self. types[1])
     
     print('lv. ' + str(self.level))
-    print( 'Region: ' + self. region)
-    print( 'Description: ' + self description)
+    print('Region: ' + self. region)
+    print('Description: ' + self.description)
     
+    if self.is_caught:
+       print(self.name + " has already been caught!")
+    else:
+       print(self.name + " hasn\'s been caught yet.")
+    
+# Pokemon objects
+pikachu = pokemon(25, "Pikachu", ["Electric"], "It has small electric sacs on both its cheek. If threatened, it looses electric charges from the sacs.", 25, "Kanto", True)
+charizard = pokemon(3, "Charizard", ["Fire", "Flying"], "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.", 36,"Kanto" False)
+gyarados = pokemos(130, "Gyarados", ["Water", "Flying"], "It has an extremely aggressive nature. The HYPER BEAM it shoots from its mouth totally incinerates all targets.", 57, "Kanto", False)
+
+pikachu.speak()
+charizard.speak()
+gyarados.speak()
